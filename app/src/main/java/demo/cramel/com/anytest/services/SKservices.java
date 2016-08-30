451b14971056bb083d8a60a0572c799c2a5145c5
xiaoping jin <jinxp@lenovo.com>
2016-08-30 17:16:10 +0800

    Add to call remote service

--- app/src/main/java/demo/cramel/com/anytest/services/SKservices.java
+++ app/src/main/java/demo/cramel/com/anytest/services/SKservices.java
@@ -2,9 +2,7 @@ package demo.cramel.com.anytest.services;
 
 import android.app.Service;
 import android.content.Intent;
-import android.os.Binder;
 import android.os.IBinder;
-import android.os.Parcel;
 import android.os.RemoteException;
 import android.util.Log;
 
@@ -19,8 +17,6 @@ import java.net.UnknownHostException;
 
 public class SKservices extends Service {
     private static final String TAG = "test";
-    String dstAddress = "10.106.11.16";
-    int dstPort = 5111;
     Socket socket = null;
     DataOutputStream out = null;
 
@@ -28,97 +24,77 @@ public class SKservices extends Service {
 
         @Override
         public void startSocket(String destIP, int port) throws RemoteException {
+            final String myIP = destIP;
+            final int myPort = port;
+            new Thread(new Runnable() {
+
+                @Override
+                public void run() {
+                    InetAddress serverAddr = null;
+                    SocketAddress sc_add = null;
+                    Log.d(TAG, "SkService start socket");
+                    try {
+                        serverAddr = InetAddress.getByName(myIP);
+                        sc_add= new InetSocketAddress(serverAddr,myPort);
+
+                        socket = new Socket();
+                        //timeout: 2000
+                        socket.connect(sc_add/*,2000*/);
+                        Log.d(TAG, "socket connect!!");
+
+                    } catch (UnknownHostException e) {
+                        Log.d(TAG, "host error : "+e);
+                    } catch (SocketException e) {
+                        Log.d(TAG,"socket error : "+e);
+                    }catch(IOException e) {
+                        Log.d(TAG,"io error : "+e);
+                    }
+                }
+            }).start();
 
         }
         @Override
         public boolean isConnnect() throws RemoteException {
             boolean result = false;
-
+            result = socket.isConnected();
             return result;
         }
         @Override
         public void sendData(byte[] data) throws RemoteException {
+            try {
+                //Log.d(TAG, "data: "+data.toString());
+                out = new DataOutputStream(socket.getOutputStream());
+                out.write(data);
+                out.flush();
 
+            } catch (IOException e) {
+                Log.d(TAG,"io error : "+e);
+            }
         }
         @Override
         public void closeSocket() throws RemoteException {
-
+            try {
+                socket.close();
+            } catch(IOException e) {
+                Log.d(TAG,"io error : "+e);
+            }
         }
     };
+
     public SKservices() {
     }
 
-    /**
-     * Class for clients to access.  Because we know this service always
-     * runs in the same process as its clients, we don't need to deal with
-     * IPC.
-     */
-    public class LocalBinder extends Binder {
-        public SKservices getService() {
-            return SKservices.this;
-        }
-    }
 
     @Override
     public IBinder onBind(Intent intent) {
-        return mBinder;
+        Log.d(TAG, "SkService onBind");
+        return serviceBinder;
     }
 
-    // This is the object that receives interactions from clients.  See
-    // RemoteService for a more complete example.
-    private final IBinder mBinder = new LocalBinder();
 
     @Override
     public void onCreate() {
-
+        Log.d(TAG, "SkService onCreate");
     }
-
-    public void sentIMUData(String data) {
-        try {
-
-            out = new DataOutputStream(socket.getOutputStream());
-            out.writeInt(0x99);
-            out.writeInt(0x67);
-            out.writeInt(0x12);
-            //Log.d(TAG, "send message : "+data);
-            out.flush();
-
-        } catch (IOException e) {
-            Log.d(TAG,"io error : "+e);
-        }
-    }
-
-    public boolean connServer() {
-        InetAddress serverAddr = null;
-        SocketAddress sc_add = null;
-
-        try {
-            serverAddr = InetAddress.getByName(dstAddress);
-            sc_add= new InetSocketAddress(serverAddr,dstPort);
-
-            socket = new Socket();
-            //timeout: 2000
-            socket.connect(sc_add/*,2000*/);
-            Log.d(TAG, "socket connect!!");
-
-        } catch (UnknownHostException e) {
-            Log.d(TAG, "host error : "+e);
-        } catch (SocketException e) {
-            Log.d(TAG,"socket error : "+e);
-        }catch(IOException e) {
-            Log.d(TAG,"io error : "+e);
-        }
-
-        return socket.isConnected();
-    }
-
-    public void disconServer() {
-        try {
-            socket.close();
-        } catch(IOException e) {
-            Log.d(TAG,"io error : "+e);
-        }
-    }
-
 
 }
